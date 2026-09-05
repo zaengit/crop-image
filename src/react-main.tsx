c8727210eecad89e97b73af69124f81f9e9e7766
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-05 19:03:12 +0700

    refactor: use bundled React DOM root

--- src/react-main.tsx
+++ src/react-main.tsx
@@ -1,6 +1,15 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './tailwind.css'
+import './style.css'
+import './enhancement.css'
 import { App } from './App'
 
 const root = document.getElementById('root')
 if (!root) throw new Error('Missing #root mount element.')
 
-ReactDOM.render(<App />, root)
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
