769381feab343011028e3e6adf580bce7b498d10
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-05 19:03:30 +0700

    chore: enable Tailwind Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command, isPreview }) => ({
   base: command === 'build' || isPreview ? '/crop-image/' : '/',
+  plugins: [tailwindcss()],
   build: {
     target: 'es2022',
   },
